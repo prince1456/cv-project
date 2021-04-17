e5a3f6fd461778e8d4e5ae469641df919bf9d2f1
NekfarBaqir <m.nekfar.baqir.786@gmail.com>
2021-04-17 15:27:26 -0800

    Inro5 input resizing solved

--- src/components/EditableInput2/EditableInput.js
+++ src/components/EditableInput2/EditableInput.js
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
 import Aux from "../../hoc/_Aux/_Aux";
+
 import classes from "./EditableInput.module.scss";
+
 const ESCAPE_KEY = 27;
 const ENTER_KEY = 13;
+
 const EditableInput = (props) => {
   const [editing, setEditing] = useState(false);
   const [input, setInput] = useState(props.placeholder);
+  const { calculateFontSize, fontSize, limit,type } = props;
   const handleEdit = () => {
     setEditing(!editing);
   };
-  const handleChange = (event) => {
-    setInput(event.target.value);
+
+  const handleChange = ({ target: { value } }) => {
+    setInput(value);
   };
+
   const handleSubmit = () => {
     let val = input?.trim();
-    if (val) {
+    if (val && val.length < limit) {
+      calculateFontSize(val,type);
       setInput(val);
       setEditing(false);
       if (props.changed) props.changed(val);
+    } else {
+      console.log("alot more character")
     }
   };
+
   const resetFields = () => {
     setInput(props.placeholder);
   };
+
   const handleKeyDown = (event) => {
     if (event.which === ESCAPE_KEY) {
       resetFields(event);
@@ -30,6 +41,7 @@ const EditableInput = (props) => {
       handleSubmit(event);
     }
   };
+
   return (
     <Aux>
       <label
@@ -38,11 +50,13 @@ const EditableInput = (props) => {
             ? [classes.hidden, classes[props.inputType]].join(" ")
             : classes[props.inputType]
         }
+        style={{ fontSize: fontSize }}
         onClick={handleEdit}
       >
         {input}
       </label>
       <input
+        style={{textAlign:"center"}}
         className={
           editing
             ? [classes[props.inputType], classes.Input].join(" ")
@@ -56,4 +70,9 @@ const EditableInput = (props) => {
     </Aux>
   );
 };
+
 export default EditableInput;
+
+EditableInput.defaultProps = {
+  fontSize: 50,
+};
--- src/containers/VideoBuilderSteps/Intro/intro5/Intro5.js
+++ src/containers/VideoBuilderSteps/Intro/intro5/Intro5.js
@@ -1,6 +1,7 @@
 import classes from "./Intro5.modules.scss";
 import VideoContainer from "../../../../components/VideoContainer/VideoContainer";
-import EditableInput from "../../../../components/EditableInput/EditableInput";
+import EditableInput from "../../../../components/EditableInput2/EditableInput";
+import { useState } from "react";
 
 const Intro5 = ({
   currentScene,
@@ -9,7 +10,42 @@ const Intro5 = ({
   data,
   bgUrl,
 }) => {
-  console.log(bgUrl, "this is the bg url ");
+  const [NamefontSize, setNameFontsize] = useState(60);
+  const [PosfontSize, setPosFontsize] = useState(60);
+
+  const calculateFontSize = (value, type) => {
+    if (type == "name") {
+      const NameCharNumber = 15;
+      if (value.length < 15) {
+        setNameFontsize(60);
+      } else if (value.length < 20) {
+        setNameFontsize(49);
+      } else if (value.length < 22) {
+        setNameFontsize(44);
+      } else if (value.length < 25) {
+        setNameFontsize(40);
+      } else {
+        console.log("to much name");
+      }
+    }
+    if (type == "position") {
+      const NameCharNumber = 15;
+      if (value.length < 15) {
+        setPosFontsize(60);
+      } else if (value.length < 20) {
+        setPosFontsize(49);
+      } else if (value.length < 22) {
+        setPosFontsize(44);
+      } else if (value.length < 25) {
+        setPosFontsize(40);
+      } else if (value.length < 30) {
+        setPosFontsize(35);
+      } else {
+        console.log("to much name");
+      }
+    }
+  };
+
   return (
     <div className={classes.Wrapper4}>
       <img src={bgUrl} />
@@ -19,27 +55,37 @@ const Intro5 = ({
         videoUpload={videoUploadHandler}
         initialFile={data.file}
       />
-      <div className={classes.TitleWrapper5}>
+      <div
+        className={classes.TitleWrapper5}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          position: "absolute",
+          right: "2%",
+          top: "40%",
+        
+        }}
+      >
         <EditableInput
           inputType="Title5"
           placeholder={data.name || "Your name here"}
           changed={(value) => changeHandler(value, "name")}
+          calculateFontSize={calculateFontSize}
+          fontSize={NamefontSize}
+          type="name"
+          limit={30}
         />
         <EditableInput
           inputType="SubTitle5"
           placeholder={data.title || "Your title here"}
           changed={(value) => changeHandler(value, "title")}
+          calculateFontSize={calculateFontSize}
+          fontSize={PosfontSize}
+          type="position"
+          limit={30}
         />
       </div>
     </div>
   );
 };
 export default Intro5;
-
-const calculateFontSize = (value, fontSize) => {
-  if (fontSize >= 50 && fontSize < 70) {
-    if (value.length > 20) {
-      return 45;
-    }
-  }
-};
